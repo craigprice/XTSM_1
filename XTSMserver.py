d17e1e8e0591ce473a33592ce8ae18da038dd6bc
= <=>
2014-09-24 22:30:43 -0400

    cleaning

--- XTSMserver.py
+++ XTSMserver.py
@@ -1659,6 +1659,8 @@ class CommandLibrary():
         plt.savefig(path+'/'+file_name+'.svg')
         plt.savefig(path+'/'+file_name+'.png')
         
+        
+        
         # mark requestor as a data generator
         #pdb.set_trace()
 
@@ -2278,17 +2280,11 @@ class GlabPythonManager():
         def pollcallback():
             if poll(): callback()
             else: return
-        if onTimeFromNow != None:
-            #pdb.set_trace()
-            reactor.callLater(onTimeFromNow, pollcallback)
-            return
-        else:
-            thistask = task.LoopingCall(pollcallback)
-            thistask.start(poll_time)
-            if not hasattr(self, "pollcallbacks"):
-                self.pollcallbacks=[]        
-            self.pollcallbacks.append(thistask)
-            return thistask
+        thistask=task.LoopingCall(pollcallback)
+        if not hasattr(self, "pollcallbacks"): self.pollcallbacks=[]        
+        self.pollcallbacks.append(thistask)
+        thistask.start(poll_time)
+        return thistask
         
     class HtmlPanel(wx.Panel):
         """
