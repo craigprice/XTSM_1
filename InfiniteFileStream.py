3df417ab0c81b202892f611a391276316fe1c0cd
craigprice-Pfaffian <=>
2014-11-25 14:58:41 -0500

    making the zip saving more robust

--- InfiniteFileStream.py
+++ InfiniteFileStream.py
@@ -14,6 +14,7 @@ import msgpack
 import cStringIO
 import zlib
 import zipfile
+import pprint
 
 DEFAULT_CHUNKSIZE=100*1000*1000
 
@@ -72,6 +73,7 @@ class FileStream_old(xstatus_ready.xstatus_ready, XTSM_Server_Objects.XTSM_Serve
         msg = msg + "This module has written the files below from the time period\n"
         msg = msg + timeheader + '\n\n'.join(self.filehistory)
         logstream.write(unicode(msg))
+        pprint.pprint(dir(self), logstream)
         logstream.close()
         
     def write(self,bytestream, keep_stream_open=False):
@@ -158,28 +160,19 @@ class Filestream(xstatus_ready.xstatus_ready, XTSM_Server_Objects.XTSM_Server_Ob
         self.defaultparams = {'zip archive created':self.init_time}
         try:
             self.location_root = file_locations.file_locations[params['file_root_selector']][uuid.getnode()]
-            self.defaultparams.update({'location_root':self.location_root+'/'+self.today+'/'})
+            self.defaultparams.update({'location_root':self.location_root+'\\'+self.today+'\\'})
         except KeyError: 
             print "error"
+            pdb.set_trace()
             raise self.UnknownDestinationError
         for key in params.keys():
             self.defaultparams.update({key:params[key]})
         for key in self.defaultparams.keys():
             setattr(self, key, self.defaultparams[key])   
-        self.location = self.location_root + str(uuid.uuid1()) + '.zip'
-        try: 
-            self.zip_file = zipfile.ZipFile(self.location,
-                                            mode='a',
-                                            compression=zipfile.ZIP_DEFLATED,
-                                            allowZip64=True)
-        except IOError: #Folder doesn't exist, then we make the day's folder.
-            os.makedirs(self.location_root)
-            self.zip_file = zipfile.ZipFile(self.location,
-                                            mode='a',
-                                            compression=zipfile.ZIP_DEFLATED,
-                                            allowZip64=True)
-        self.filehistory = [self.location]
-        print self.location
+        self.logstream = io.open(self.location_root + 'filestream_log.txt', 'a')
+        self.logstream.write(unicode('This is a log of file writes\n'))
+        self.root_zip_name = str(uuid.uuid1()) + '.zip'
+        print self.location_root
 
     class UnknownDestinationError(Exception):
         pass
@@ -188,57 +181,55 @@ class Filestream(xstatus_ready.xstatus_ready, XTSM_Server_Objects.XTSM_Server_Ob
         """
         outputs a log of recently written files
         """
-        logstream = io.open(self.location_root + 'filestream_log.txt', 'a')
+        self.logstream = io.open(self.location_root + 'filestream_log.txt', 'a')
         time_format = '%Y-%m-%d %H:%M:%S'
         time1 = datetime.datetime.fromtimestamp(self.init_time).strftime(time_format)
-        time2 = datetime.datetime.fromtimestamp(time.time()).strftime(time_format)
-        timeheader= time1 + " through "+ time2
-        msg = "\nThis is a log of file writes: This module has written the"
-        msg = msg + "files below from the time period\n"
-        msg = msg + timeheader + '\n\n'.join(self.filehistory)
-        logstream.write(unicode(msg))
-        logstream.close()
+        timeheader= time1
+        msg = "This module has written,\n"
+        msg = msg + self.zip_file_name + '\\' + self.fileName + '\n'
+        msg = msg + "at time, " + timeheader + '\n'
+        self.logstream.write(unicode(msg))
+        #pprint.pprint(unicode(dir(self)), logstream)
+        self.logstream.close()
         
-    def write_file(self, msg, comments='', prefix='', extension='.dat'):
+    def _write_file(self, msg, comments='', prefix='', extension='.dat', is_backup=False):
         """
         writes a file to the zip archive.
         """
-        fileName = str(prefix) + str(uuid.uuid1()) + str(extension)
-        info = zipfile.ZipInfo(fileName, date_time=time.localtime(time.time()))
+        if is_backup:
+            self.zip_file_name = self.location_root + 'Backup_' + self.root_zip_name
+        else:
+            self.zip_file_name = self.location_root + self.root_zip_name
+            
+        try: 
+            self.zip_file = zipfile.ZipFile(self.zip_file_name,
+                                            mode='a',
+                                            compression=zipfile.ZIP_DEFLATED,
+                                            allowZip64=True)
+        except IOError: #Folder doesn't exist, then we make the day's folder.
+            os.makedirs(self.location_root)
+            self.zip_file = zipfile.ZipFile(self.zip_file_name,
+                                            mode='a',
+                                            compression=zipfile.ZIP_DEFLATED,
+                                            allowZip64=True)
+        self.fileName = str(prefix) + str(uuid.uuid1()) + str(extension)
+        info = zipfile.ZipInfo(self.fileName, date_time=time.localtime(time.time()))
         info.compress_type = zipfile.ZIP_DEFLATED
         info.comment = comments + str(self.defaultparams)
         self.zip_file.writestr(info, msg)
-        return self.location + "/" + fileName
-        
-        
-    def __flush__(self):
         self.zip_file.close()
         self.output_log()
+        return self.zip_file_name + "/" + self.fileName
         
-            
-def open_file(path):
-    fileName = 'c:/wamp/www/raw_buffers/DBFS/2014-10-13/6ea6bf2e-52fe-11e4-b225-0010187736b5.msgp'
-    import zlib
-    import cStringIO
-    import zipfile
-    zf = zipfile.ZipFile(fileName, 'r')
-    print zf.namelist()
-    for info in zf.infolist():
-        print info.filename
-        print '\tComment:\t', info.comment
-        print '\tModified:\t', datetime.datetime(*info.date_time)
-        print '\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)'
-        print '\tZIP version:\t', info.create_version
-        print '\tCompressed:\t', info.compress_size, 'bytes'
-        print '\tUncompressed:\t', info.file_size, 'bytes'
-        print
-    #info = zf.getinfo(filename)
-    #data = zf.read(filename)
-    f = open(fileName,'rb')
-    c = zlib.decompressobj()
-    cBlock = c.decompress(f.read())
-    print cBlock
-    output = cStringIO.StringIO(cBlock)
-    unpacker = msgpack.Unpacker(output,use_list=False)# If data was msgpacked
-    print unpacker.next()
-    print cBlock
+        
+    def write_file(self, msg, comments='', prefix='', extension='.dat', is_backup=False):
+        """
+        writes a file to the zip archive.
+        """
+        self._write_file(msg, comments=comments, prefix='Backup_'+prefix, extension=extension, is_backup=True)
+        self.check_todays_files()
+        return self._write_file(msg, comments=comments, prefix=prefix, extension=extension, is_backup=False)
+        
+    def __flush__(self):
+        pass
+        
